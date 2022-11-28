4fe160d68a5fc552b1431ee9d48dcbab9fc54263
DELL <DELL@DESKTOP-RRGV49A>
2022-11-28 20:02:36 +0000

    commited to eclipse

---
+++ src/test/java/Test1_Welcome.java
@@ -0,0 +1,15 @@
+// Test1 : This is just a welcome program using Java
+
+
+
+public class Test1_Welcome {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("welcome to the first batch");
+		
+	}
+
+}
